767e2604373026c5467978a9bca797eb6b564a52
Venay <silver0snake@gmail.com>
2020-06-05 20:45:24 +0100

    base

---
+++ Assets/InputDebugger.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+namespace Testing.InputDebugger
+{
+	public class InputDebugger : MonoBehaviour
+	{
+		PointerInput pointerInput;
+		public Text inputText;
+		public Text deviceText;
+		public Text buttonText;
+
+		private void Awake()
+		{
+			pointerInput = new PointerInput();
+			pointerInput.General.Pointer.performed += ctx => OnPointer(ctx.ReadValue<myCompositData>());
+			pointerInput.General.Pointer.canceled += ctx => OnPointer(ctx.ReadValue<myCompositData>());
+
+			string text = "";
+
+			foreach (var S in InputSystem.devices)
+			{
+				text += S.device + " : " + S.enabled + "  \n";
+			}
+			deviceText.text = text;
+		}
+
+		private void OnEnable() => pointerInput.Enable();
+		private void OnDisable() => pointerInput.Disable();
+
+
+
+		public void OnPointer(myCompositData data)
+		{
+
+			string n = "\n";
+			string Press = "Press: " + data.Press.ToString() + n;
+			string PressCount = "Press Count: " + data.PressCount + n;
+			string Position = "Position: " + data.Position + n;
+			string Delta = "Delta: " + data.Delta + n;
+			string Radius = "Radius: " + data.Radius + n;
+
+			string Tap = "Tap: " + data.Tap + n;
+			string StartPosition = "Start Position: " + data.StartPosition + n;
+			string StartTime = "Start Time: " + data.StartTime + n;
+
+			string Scroll = "Scroll: " + data.Scroll + n;
+
+			inputText.text = Time.time + n + Press + PressCount + Position + Delta + Radius + n + Tap + StartPosition + StartTime + n + Scroll;
+
+		}
+
+		public void OnButtonPress() => buttonText.text = "Pressed at:\n" + Time.time;
+	}
+
+
+}
---
+++ Assets/myComposit.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.Layouts;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public struct myCompositData
+{
+	public bool Press;
+	public int PressCount;
+	public Vector2 Position;
+	public Vector2 Delta;
+	public Vector2 Radius;
+
+	public bool Tap;
+	//public UnityEngine.InputSystem.TouchPhase Phase;
+	public Vector2 StartPosition;
+	public double StartTime;
+
+	public Vector2 Scroll;
+}
+
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class myComposit : InputBindingComposite<myCompositData>
+{
+	
+
+#if UNITY_EDITOR
+	static myComposit()
+	{
+		Initialize();
+	}
+#endif
+
+
+	[RuntimeInitializeOnLoadMethod]
+	private static void Initialize()
+	{
+		InputSystem.RegisterBindingComposite<myComposit>();
+	}
+
+	[InputControl(layout = "Button")]
+	public int press;
+	[InputControl(layout = "Integer")]
+	public int pressCount;
+	[InputControl(layout = "Vector2")]
+	public int position;
+	[InputControl(layout = "Vector2")]
+	public int delta;
+	[InputControl(layout = "Vector2")]
+	public int radius;
+
+	[InputControl(layout = "Button")]
+	public int tap;
+	//[InputControl(layout = "TouchPhase")]
+	//public int phase;
+	[InputControl(layout = "Vector2")]
+	public int startPosition;
+	[InputControl(layout = "Double")]
+	public int startTime;
+
+	[InputControl(layout = "Vector2")]
+	public int scroll;
+
+	
+
+	public override myCompositData ReadValue(ref InputBindingCompositeContext context)
+	{
+		
+		var press = context.ReadValueAsButton(this.press);
+		var pressCount = context.ReadValue<int>(this.pressCount);
+		var position = context.ReadValue<Vector2, Vector2MagnitudeComparer>(this.position);
+		var delta = context.ReadValue<Vector2, Vector2MagnitudeComparer>(this.delta);
+		var radius = context.ReadValue<Vector2, Vector2MagnitudeComparer>(this.radius);
+
+		var tap = context.ReadValueAsButton(this.tap);
+		//var phase = context.ReadValue<UnityEngine.InputSystem.TouchPhase>(this.phase);
+		var startPosition = context.ReadValue<Vector2, Vector2MagnitudeComparer>(this.startPosition);
+		var startTime = context.ReadValue<double>(this.startTime);
+
+		var scroll = context.ReadValue<Vector2, Vector2MagnitudeComparer>(this.scroll);
+
+		return new myCompositData()
+		{
+			Press = press,
+			PressCount = pressCount,
+			Position = position,
+			Delta = delta,
+			Radius = radius,
+
+			Tap = tap,
+			StartPosition = startPosition,
+			StartTime = startTime,
+
+			Scroll = scroll
+
+		};
+	}
+}
